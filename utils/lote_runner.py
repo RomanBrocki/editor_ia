2170ccac6ff7c7f35c14851e2c49b0578fb524c1
RomanBrocki <romanbrocki@hotmail.com>
2025-06-07 01:00:01 -0300

    deleção de arquivo inútil

--- utils/lote_runner.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def revisar_com_subprocesso(nome_arquivo: str):
-    """Reinicia o app.py passando o nome do arquivo como argumento"""
-    subprocess.run(["python", "app.py", nome_arquivo])
-
